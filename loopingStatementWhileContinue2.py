4c9971eca6432a06297ca6a2c141c9547de40a6c
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-25 22:50:09 +0530

    Add files via upload

---
+++ loopingStatementWhileContinue2.py
@@ -0,0 +1,8 @@
+while True:
+    name=input('Enter user name:')
+    if name !='Rahul':
+        continue
+    password=input('Hello , Rahul. What is the password?')
+    if password !='Singh':
+        break
+    print('Access Granted')
