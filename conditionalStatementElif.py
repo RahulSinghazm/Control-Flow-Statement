d790b5cda82d0b9dff47ae34b2606b351092e1de
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-25 22:16:47 +0530

    Add files via upload

---
+++ conditionalStatementElif.py
@@ -0,0 +1,12 @@
+print('Begin')
+a=input("Enter positive no.:")
+b=int(a)
+if b<10:
+    print('Given no. is one digit no.')
+elif b<100:
+    print('Given no. is two digit no.')
+elif b<1000:
+    print('Given no. is three digit no.')
+else:
+    print('Given no. is >= four digit no.')
+print('End')
