82ec72217b2e6b1098f4a822377b84951d871200
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-26 23:44:23 +0530

    Update conditionalStatementElif.py

--- conditionalStatementElif.py
+++ conditionalStatementElif.py
@@ -4,7 +4,7 @@ b=int(a)
 if b<10:
     print('Given no. is one Digit no.')
 elif b<100:
-    print('Given no. is two digit no.')
+    print('Given no. is two Digit no.')
 elif b<1000:
     print('Given no. is three Digit no.')
 else:
