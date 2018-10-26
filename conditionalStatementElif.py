6107cfa7e7ee1c6df91c37d17a113880fa756436
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-26 23:43:30 +0530

    Update conditionalStatementElif.py

--- conditionalStatementElif.py
+++ conditionalStatementElif.py
@@ -2,7 +2,7 @@ print('Begin')
 a=input("Enter Positive no.:")
 b=int(a)
 if b<10:
-    print('Given no. is one digit no.')
+    print('Given no. is one Digit no.')
 elif b<100:
     print('Given no. is two digit no.')
 elif b<1000:
