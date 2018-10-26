ab335468e1319f6f135f80003a9e6d097b82749f
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-26 23:45:00 +0530

    Update conditionalStatementElse.py

--- conditionalStatementElse.py
+++ conditionalStatementElse.py
@@ -2,7 +2,7 @@ print('Begin')
 a=input('Enter Positive no.:')
 b=int(a)
 if b<10:
-    print('Given no. is one digit no.')
+    print('Given no. is one Digit no.')
 else:
     print('Given no. is >= two digit no.')
 print('End')
