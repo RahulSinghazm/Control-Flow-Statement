36f2c938db3e6bcee15c1e275898e47ab89e7de6
Rahul Singh <31289155+RahulSinghazm@users.noreply.github.com>
2018-10-28 19:50:02 +0530

    Update loopingStatementWhileContinue2.py

--- loopingStatementWhileContinue2.py
+++ loopingStatementWhileContinue2.py
@@ -1,6 +1,7 @@
 while True:
     name=input('Enter User Name:')
     if name !='Rahul':
+        print('Enter the correct User Name')
         continue
     password=input('Hello , Rahul. What is the Password?')
     if password !='Singh':
